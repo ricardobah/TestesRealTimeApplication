5cab737d5b281afc10166e8a8835d0be0369ea1a
ricardobah <rickribeiro99@gmail.com>
2018-09-19 09:09:02 -0300

    Application tests to project

---
+++ server.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+app.use(express.static(path.join(__dirname,'public')));
+app.set('views',path.join(__dirname,'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/',(req,res) => {
+res.render('index.html');
+
+});
+
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado : ${socket.id}`);
+    console.log(messages);
+   
+    
+
+        socket.on('sendMessage', data =>{
+        messages.push(data);
+        console.log(messages);
+        socket.broadcast.emit('recivedMessage',data);
+        });
+    
+});
+
+server.listen(3000);
+
